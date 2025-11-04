fc73062699b3e22e8109d5ee67fd220b3e1f969a
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-11-04 12:58:11 +0000

    make sure xts is loaded (not attached) onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,14 @@ NULL
 #' @useDynLib rsplash, .registration = TRUE
 ## usethis namespace: end
 NULL
+
+.onLoad <- function(libname, pkgname) {
+  # Make sure xts namespace is loaded when your package is loaded.
+  # This does not attach xts to the search path (unlike Depends),
+  # but it registers the xts class so saved xts objects in data/ will be restored correctly.
+  if (!requireNamespace("xts", quietly = TRUE)) {
+    packageStartupMessage(
+      "Package 'xts' is required to restore data objects with xts classes."
+    )
+  }
+}
